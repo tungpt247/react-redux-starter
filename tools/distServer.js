223e2acb34312e6cc1b8458e25b3dd0b44b6ce16
tungpt247 <tungpt247@gmail.com>
2016-06-29 17:03:51 +0700

    Improve loading page

--- tools/distServer.js
+++ tools/distServer.js
@@ -1,15 +1,23 @@
-import express from 'express'
-import path from 'path'
-import open from 'open'
-import compression from 'compression'
-
-/*eslint-disable no-console */
+import express        from 'express'
+import path           from 'path'
+import open           from 'open'
+import compression    from 'compression'
+import methodOverride from 'method-override'
 
 const port = 3000
 const app = express()
 
 app.use(compression())
+app.use(methodOverride());
 app.use(express.static('dist'))
+
+// app.get('*', function(req, res, next) {
+//   if (req.url.indexOf('/images/') === 0 || req.url.indexOf('/main.js/') === 0) {
+//     res.setHeader('Cache-Control', 'public, max-age=2592000')
+//     res.setHeader('Expires', new Date(Date.now() + 2592000000).toUTCString())
+//   }
+//   next()
+// })
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '../dist/index.html'))
